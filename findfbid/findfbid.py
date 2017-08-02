a025c42507ad7f9bce8525396c9a16ca9726bcab
anhngo5312 <anhvjp123z@gmail.com>
2017-08-02 14:30:56 +0700

    update code
    fix some error

--- findfbid/findfbid.py
+++ findfbid/findfbid.py
@@ -9,20 +9,26 @@ app = Flask(__name__)
 def hello():
     return 'Test OK'
 
-@app.route('/findfbid', methods=['GET'])
+@app.route('/findfbid', methods=['GET', 'POST'])
 def findfbid():
-    url = request.args.get('url')
-    f = urllib.request.urlopen(url)
-    content = f.read()
-    soup = BeautifulSoup(content, 'html.parser')
-    temp = soup.find('meta', attrs={'property': 'al:android:url'})
-    temp = str(temp).split('\" ')[0]
-    temp = temp.split('/')[-1]
-    data = {}
-    data['fbid'] = temp
-    json_data = json.dumps(data)
-    return json_data
+    try :
+        url = request.args.get('url')
+        f = urllib.request.urlopen(url)
+        content = f.read()
+        soup = BeautifulSoup(content, 'html.parser')
+        temp = soup.find('meta', attrs={'property': 'al:android:url'})
+        temp = str(temp).split('\" ')[0]
+        temp = temp.split('/')[-1]
+        data = {}
+        data['fbid'] = temp
+        json_data = json.dumps(data)
+        return json_data
+    except :
+        data = {}
+        data['fbid'] = 'none'
+        json_data = json.dumps(data)
+        return json_data
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5000)))
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
 
